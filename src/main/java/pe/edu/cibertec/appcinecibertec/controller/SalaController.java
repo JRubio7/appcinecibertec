7e386b3bfdb61e8cefc7c702a0d3858790fa91f2
JRubio7 <i202114280@cibertec.edu.pe>
2023-05-21 11:54:16 -0500

    controller sala

--- src/main/java/pe/edu/cibertec/appcinecibertec/controller/SalaController.java
+++ src/main/java/pe/edu/cibertec/appcinecibertec/controller/SalaController.java
@@ -1,14 +1,19 @@
 package pe.edu.cibertec.appcinecibertec.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import pe.edu.cibertec.appcinecibertec.model.bd.Estado;
+import pe.edu.cibertec.appcinecibertec.model.bd.Sala;
 import pe.edu.cibertec.appcinecibertec.model.request.SalaRequest;
 import pe.edu.cibertec.appcinecibertec.model.response.ResultadoResponse;
 import pe.edu.cibertec.appcinecibertec.service.SalaService;
@@ -23,17 +28,62 @@ public class SalaController {
 	@GetMapping("/frmsala")
 	public String frmMantSala(Model model) {
 		
-		model.addAttribute("listaEstado", salaService.listarSala());
-		return "Estado/frmMantEstado";
+		model.addAttribute("listaSala", salaService.listarSala());
+		return "sala/frmMantSala";
 	}
 	
 	@PostMapping("/registrarSala")
-	@ResponseBody //indica que va a devolver un objeto cualquiera menos un string
+	@ResponseBody //indica que va a devolver un objeto usando AJAX
 	public ResultadoResponse registrarSala(
 			@RequestBody SalaRequest salaRequest
 			){
 		
-		return null;
+		String mensaje = "Sala registrada correctamente";
+		Boolean respuesta = true;
+		
+		try {
+			Sala objSala = new Sala();
+			if(salaRequest.getIdsala() > 0) {
+				objSala.setIdsala(salaRequest.getIdsala());
+			}
+			objSala.setDescsala(salaRequest.getDescsala());
+			objSala.setAsientos(salaRequest.getAsientos());
+			Estado objEstado = new Estado();
+			objEstado.setIdEstado(salaRequest.getIdestado());
+			objSala.setEstado(objEstado);
+			salaService.registrarSala(objSala);
+
+		}catch(Exception ex){
+			mensaje = "Sala no registrada";
+			respuesta = false;
+		}
+		return ResultadoResponse.builder().mensaje(mensaje).respuesta(respuesta).build();
 	}
+	
+	@DeleteMapping("/eliminarSala")
+	@ResponseBody
+	public ResultadoResponse eliminarSala(
+			@RequestBody
+			SalaRequest salaRequest) {
+		
+		String mensaje = "Sala eliminada correctamente";
+		Boolean respuesta = true;
+		
+		try {
+			salaService.eliminarSala(salaRequest.getIdestado());
+		} catch (Exception e) {
+			mensaje = "Sala no eliminada";
+			respuesta = false;
+		}
+		return ResultadoResponse.builder().mensaje(mensaje).respuesta(respuesta).build();
+		
+	}
+	
+	@GetMapping("listarSalas")
+	@ResponseBody
+	public List<Sala> listarSalas(){
+		return salaService.listarSala();
+	}
+	
 
 }
